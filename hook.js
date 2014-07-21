474bbdeb2f5595c16e0206c8c156b4eedf1af83d
spaceinvaderz <anton.seledets@gmail.com>
2014-07-22 00:13:26 +0400

    Some fixes 3

--- hook.js
+++ hook.js
@@ -4,10 +4,10 @@ var branch = 'master'
 var gith = require('gith').create( port );
 // Import execFile, to run our bash script
 var execFile = require('child_process').execFile;
-console.log('Webhook process is running on port' + port);
+console.log('Webhook process is running on port: ' + port);
 
 gith({
-	"repo": "Spaceinvaderz/webhooks"
+	repo: 'Spaceinvaderz/webhooks'
 }).on( 'all' , function( payload ) {
 	if(payload.branch === branch)
 	{
